3572bf3077f1582a8d200b2d9de0fdea9017def3
GauravSJawla <gauravsinghjawla@gmail.com>
2020-03-29 01:26:47 -0500

    Initial connection of Front & backend working

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import { createBrowserHistory } from 'history';
-import { Router, Route, Switch } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import 'assets/scss/material-kit-react.scss?v=1.8.0';
 
 // pages for this product
-import Components from 'views/Components/Components.js';
 import LandingPage from 'views/LandingPage/LandingPage.js';
 import ProfilePage from 'views/ProfilePage/ProfilePage.js';
 import LoginPage from 'views/LoginPage/LoginPage.js';
 
-var hist = createBrowserHistory();
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <Router history={hist}>
-      <Switch>
-        <Route path='/landing-page' component={LandingPage} />
-        <Route path='/profile-page' component={ProfilePage} />
-        <Route path='/login-page' component={LoginPage} />
-        <Route path='/' component={Components} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/profile-page' component={ProfilePage} />
+            <Route exact path='/login-page' component={LoginPage} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
